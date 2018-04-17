ba1d8afb2c6e07f1eeca40c9b0ae25a0a9547503
wallstedt <fredrik.wallstedt@gmail.com>
2018-04-17 03:30:36 +0200

    Remove unused blocks

--- src/index.js
+++ src/index.js
@@ -10,83 +10,11 @@ import * as connectionHelpers from './lib/connectionHelpers'
 document.registeredComponents = {}
 document.nextId = 0
 
-class Component {
-  constructor() {
-    this._id = ++document.nextId;
-    document.registeredComponents[this._id] = this;
-  }
-}
-
-class Login extends Component {
-  constructor(history){
-    super()
-    this.history = history
-
-    console.log(document.registeredComponents[this._id]);
-  }
-  render(){
-    return `
-    <div>
-      <p>THe player is logged in now</p>
-      <a id="logout" href="/logout" onclick="document.registeredComponents[${this._id}].logout()">Logout</a>
-    </div>
-    `;
-  }
-    logout(){
-    // remove the session
-    window.localStorage.clear();
-
-    // send the user to the rootpath
-    this.history.replace('/')
-  }
-} // end Login
-
-class Landing_old extends Component {
-  constructor(history) {
-    super();
-    this.history = history;
-  }
-  render() {
-    return `
-    <div>
-      <p>The user logs in by pressing the button</p>
-      <p>The button below symbolizes the complete logging in process and will be replaced by:</p>
-      <ul>
-        <li>A form</li>
-        <li>setBody() will be replaced by a function consuming utility functions for logging in</li>
-      </ul>
-      <p>In addition, upon loading the app, localstorage state will be loaded, and injected (NOT IMPOTANT, BUT NICE)</p>
-      <button onclick="document.registeredComponents[${
-        this._id
-      }].setBody(this.value)">Klick</button>      
-    </div>
-    `;
-  }
-
-  setBody(newBody) {
-    // save state, then push
-    saveState({
-      auth: {
-        // get these from the request
-        id: '1',
-        username: 'demouser1',
-        token: 'madeup'
-      }
-    });
-
-    this.history.push('/login', { auth: true });
-
-    /* const obj = { path: '/login' }
-    history.pushState(obj, 'doctitle', '/login') */
-  }
-} // end Landing
-
-
 window.onload = () => {
   const history = createHistory();
 
   const landing = new Landing(history);
-  const login = new Login(history);
+  /* const login = new Login(history); */
   let sites = null;
 
   const state = loadState();
